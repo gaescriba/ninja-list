7e8a4f0afc9e526c85d1e918f110e8d642d1743b
gaescriba <gaescriba.trabajo@gmail.com>
2021-07-24 13:46:10 -0400

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            <p>Copyright 2021 ninja list</p>
+        </footer>
+     );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+const About = () => {
+    return ( 
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto illo tempore reiciendis? Quam optio perspiciatis soluta tenetur nisi, excepturi porro accusantium eos nulla, eaque magnam facilis nobis rerum sapiente voluptate.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At quam praesentium rerum soluta debitis quidem fugiat, facere, laboriosam id, dolorum consequatur! Non, aperiam. Architecto reprehenderit unde dicta laboriosam quidem ducimus!</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
+
